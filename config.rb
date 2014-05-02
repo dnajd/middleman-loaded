de74f934fcf9e11ab4ea4b0b8d1e5b25a6d4ef95
Don Najd <dnajd7@gmail.com>
2014-05-01 22:32:07 -0700

    build before project pages deploy

--- config.rb
+++ config.rb
@@ -23,6 +23,7 @@ activate :syntax # code highlighting
 # github project pages deploy
 #activate :deploy do |deploy|
 #  deploy.method = :git
+#  deploy.build_before = true # default: false
 #end
 
 # markdown settings
